1bb42f9e2ce86b564c3a9e48363529ef367a01ae
gbevi <gbevi281@gmail.com>
2024-12-04 13:19:02 -0300

    feat(api): adição de Rate Limiters e CORS

--- Back-end/src/app.module.ts
+++ Back-end/src/app.module.ts
@@ -3,10 +3,33 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { UsersModule } from './users/users.module';
+import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
-  imports: [DatabaseModule, UsersModule],
+  imports: [
+    DatabaseModule,
+    UsersModule,
+    ThrottlerModule.forRoot([
+      {
+        name: 'short',
+        ttl: 100,
+        limit: 3,
+      },
+      {
+        name: 'long',
+        ttl: 6000,
+        limit: 100,
+      },
+    ]),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
