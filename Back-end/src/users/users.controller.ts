ca934044768d0f4ef30c556ddb46247f9cf12e9d
gbevi <gbevi281@gmail.com>
2024-12-04 14:28:11 -0300

    feat(users): adição de erros detalhados por meio do logger

--- Back-end/src/users/users.controller.ts
+++ Back-end/src/users/users.controller.ts
@@ -1,36 +1,55 @@
-// eslint-disable-next-line prettier/prettier
-import { Controller, Get, Post, Body, Patch, Param, Delete, ParseUUIDPipe, ValidationPipe } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  ParseUUIDPipe,
+  ValidationPipe,
+  Query,
+  Ip,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/Create-user.dto';
 import { UpdateUserDto } from './dto/Update-user.dto';
+import { MyLoggerService } from 'src/my-logger/my-logger.service';
+import { Role } from '@prisma/client';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+  private readonly logger = new MyLoggerService(UsersController.name);
 
   @Post()
-  create(@Body(ValidationPipe) createUserDto: CreateUserDto) {
+  create(@Ip() ip: string, @Body(ValidationPipe) createUserDto: CreateUserDto) {
+    this.logger.log(`Request for all users\t${ip}`, UsersController.name);
     return this.usersService.create(createUserDto);
   }
 
   @Get()
-  findAll() {
-    return this.usersService.findAll();
+  findAll(@Ip() ip: string, @Query('role') role?: Role) {
+    this.logger.log(`Request for all users\t${ip}`, UsersController.name);
+    return this.usersService.findAll(role);
   }
 
   @Get(':id')
-  findOne(@Param('id', ParseUUIDPipe) id: string) {
+  findOne(@Ip() ip: string, @Param('id', ParseUUIDPipe) id: string) {
+    this.logger.log(`Request for all users\t${ip}`, UsersController.name);
     return this.usersService.findOne(id);
   }
 
   @Patch(':id')
   // eslint-disable-next-line prettier/prettier
-  update(@Param('id', ParseUUIDPipe) id: string, @Body(ValidationPipe) updateUserDto: UpdateUserDto) {
+  update(@Ip() ip: string, @Param('id', ParseUUIDPipe) id: string, @Body(ValidationPipe) updateUserDto: UpdateUserDto) {
+    this.logger.log(`Request for all users\t${ip}`, UsersController.name);
     return this.usersService.update(id, updateUserDto);
   }
 
   @Delete(':id')
-  remove(@Param('id', ParseUUIDPipe) id: string) {
+  remove(@Ip() ip: string, @Param('id', ParseUUIDPipe) id: string) {
+    this.logger.log(`Request for all users\t${ip}`, UsersController.name);
     return this.usersService.remove(id);
   }
 }
