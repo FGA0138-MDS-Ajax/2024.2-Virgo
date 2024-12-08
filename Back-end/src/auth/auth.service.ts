ecdc4237b63cf144c15cdab50ebab5987948a035
PedrooCamilo <pedro2005camilo@gmail.com>
2024-12-08 13:52:32 -0300

    feat(auth): implement local strategy validation

--- Back-end/src/auth/auth.service.ts
+++ Back-end/src/auth/auth.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { UsersService } from 'src/users/users.service';	
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(private readonly userService: UsersService) {}
+    async validateUser(email: string, password: string) {
+        const user = await this.userService.findByEmail(email);
+    }
+}
