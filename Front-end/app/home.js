13d3584a406e8b3f15bcedd24470a59914eb7a6b
joaolobo10 <joaocarloslobo10@gmail.com>
2024-12-17 19:40:15 -0300

    fix: página de bem vindo para teste, alterado

--- Front-end/app/home.js
+++ Front-end/app/home.js
@@ -1,13 +1,25 @@
-import { Ionicons } from '@expo/vector-icons';
-import { useRouter } from 'expo-router';
-import React, { useState } from 'react';
-import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
-
-export default function home() {
+export default function Home() {
   return (
-    <View>
-      <Text>Salve</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Bem-Vindo ao Cuidar Verde!</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1, // Faz o componente ocupar toda a tela
+    backgroundColor: "#E8F5E9", // Cor verde clara suave
+    justifyContent: "center", // Centraliza no eixo vertical
+    alignItems: "center", // Centraliza no eixo horizontal
+    padding: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "#2E7D32", // Um tom de verde escuro
+  },
+});
